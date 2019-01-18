ddc3190996307afe0e2bc452c886cdd8a14a5fc1
Ram Thiru <thirunsk@rose-hulman.edu>
2019-01-17 20:40:42 -0500

    m7 todo-s done

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Ram Thiru.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,3 +43,16 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow
+bessy = rg.SimpleTurtle()
+bessy.pen = rg.Pen('blue',20)
+bessy.left(90)
+bessy.forward(200)
+bessy.pen_up()
+bessy.go_to(rg.Point(100,-40))
+bessy.pen_down()
+bessy.pen = rg.Pen('green',10)
+bessy.backward(150)
+window.close_on_mouse_click()
+
